7b745df1e8a90a1cb43f89f4965b4f130a74a22f
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2021-12-14 21:18:51 +0500

    heroku

--- visualshop/settings.py
+++ visualshop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG')
 ALLOWED_HOSTS = ['visualshopp.herokuapp.com','visual-shop.vercel.app','localhost','http://localhost:3000','127.0.0.1','http://192.168.10.17:3000']
 
 
