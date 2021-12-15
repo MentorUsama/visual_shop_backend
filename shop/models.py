015d449958c23d271bf94cc656cce5342c9821a6
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2021-12-15 13:21:28 +0500

    products added

--- shop/models.py
+++ shop/models.py
@@ -74,20 +74,20 @@ class Images(models.Model):
 
 
 # ==================== Signals for Image deleted or updated =============
-@receiver(models.signals.post_delete, sender=Images)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
-@receiver(models.signals.pre_save, sender=Images)
-def auto_delete_file_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return False
-    try:
-        old_file = Images.objects.get(pk=instance.pk).image
-    except object.DoesNotExist:
-        return False
-    new_file = instance.image
-    if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
+# @receiver(models.signals.post_delete, sender=Images)
+# def auto_delete_file_on_delete(sender, instance, **kwargs):
+#     if instance.image:
+#         if os.path.isfile(instance.image.path):
+#             os.remove(instance.image.path)
+# @receiver(models.signals.pre_save, sender=Images)
+# def auto_delete_file_on_change(sender, instance, **kwargs):
+#     if not instance.pk:
+#         return False
+#     try:
+#         old_file = Images.objects.get(pk=instance.pk).image
+#     except object.DoesNotExist:
+#         return False
+#     new_file = instance.image
+#     if not old_file == new_file:
+#         if os.path.isfile(old_file.path):
+#             os.remove(old_file.path)
