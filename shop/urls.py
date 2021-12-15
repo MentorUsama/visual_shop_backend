dc4ce460ad9a0561cf1ed268ec94d51980e58c97
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2021-12-16 00:03:51 +0500

    get product by detail and category added

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('getProducts/', views.GetProductsAPI.as_view(),name='getProduct'),
+    path('products/', views.GetProductsAPI.as_view(),name='getProduct'),
+    path('product/<pk>',views.GetProductDetail.as_view(),name="productDetail"),
+    path('products/category/<pk>',views.GetProductByCategory.as_view(),name="getProductByCategory"),
+    path('products/category/<category>/subcategory/<subcategory>',views.GetProductBySubCategory.as_view(),name="getProductBySubCategory")
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
+from django.http import Http404
 from .serialization import ProductSerializer
-from rest_framework.generics import ListAPIView
+from .models import Product
+from rest_framework.generics import ListAPIView,RetrieveAPIView
+from rest_framework.views import APIView
 # getting models
 from .models import Product
+# from rest_framework import BasicAuthentication
+from visualshop.utility.request import Success
 # Create your views here.
 class ProductPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
@@ -16,3 +21,23 @@ class GetProductsAPI(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
+class GetProductDetail(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class GetProductByCategory(ListAPIView):
+    serializer_class = ProductSerializer
+    model = Product
+    pagination_class = ProductPagination
+    def get_queryset(self):
+        uid = self.kwargs.get('pk')
+        queryset = self.model.objects.filter(subCategoryId__categoryId=uid)
+        return queryset
+class GetProductBySubCategory(ListAPIView):
+    serializer_class = ProductSerializer
+    model = Product
+    pagination_class = ProductPagination
+    def get_queryset(self):
+        category = self.kwargs.get('category')
+        subcategory=self.kwargs.get('subcategory')
+        queryset = self.model.objects.filter(subCategoryId=subcategory,subCategoryId__categoryId=category)
+        return queryset
