8f571a17066ea548179625baa1ec88db5fcf4c90
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2021-12-17 14:11:36 +0500

    Search Product added

--- shop/urls.py
+++ shop/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('products/', views.GetProductsAPI.as_view(),name='getProduct'),
     path('product/<pk>',views.GetProductDetail.as_view(),name="productDetail"),
     path('products/category/<pk>',views.GetProductByCategory.as_view(),name="getProductByCategory"),
-    path('products/category/<category>/subcategory/<subcategory>',views.GetProductBySubCategory.as_view(),name="getProductBySubCategory")
+    path('products/category/<category>/subcategory/<subcategory>',views.GetProductBySubCategory.as_view(),name="getProductBySubCategory"),
+    path('products/search/<str:text>',views.GetProductByText.as_view(),name="GetProductByText")
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
-from django.http import Http404
+from django.contrib.postgres.search import SearchQuery,SearchRank,SearchVector
 from .serialization import ProductSerializer
 from .models import Product
 from rest_framework.generics import ListAPIView,RetrieveAPIView
@@ -41,3 +41,16 @@ class GetProductBySubCategory(ListAPIView):
         subcategory=self.kwargs.get('subcategory')
         queryset = self.model.objects.filter(subCategoryId=subcategory,subCategoryId__categoryId=category)
         return queryset
+
+class GetProductByText(ListAPIView):
+    serializer_class = ProductSerializer
+    model = Product
+    pagination_class = ProductPagination
+    def get_queryset(self):
+        text = self.kwargs.get('text')
+        search_vector = SearchVector("tags__name",weight='A') + SearchVector('name', weight='B') + SearchVector('description', weight='C')
+        search_query = SearchQuery(text)
+        results = Product.objects.annotate(
+            rank=SearchRank(search_vector, search_query)
+        ).filter(rank__gte=0.3).order_by('-rank')
+        return results
