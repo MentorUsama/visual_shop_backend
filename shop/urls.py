06caab8ab50089eac9defc33384f09deef78c000
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-01-16 23:39:30 +0500

    FilterProduct API Added

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('products/', views.GetProductsAPI.as_view(),name='getProduct'),
     path('product/<pk>',views.GetProductDetail.as_view(),name="productDetail"),
+    path('filterProducts',views.FilterProduct.as_view(),name="FilterProduct"),
     path('products/category/<pk>',views.GetProductByCategory.as_view(),name="getProductByCategory"),
     path('products/category/<category>/subcategory/<subcategory>',views.GetProductBySubCategory.as_view(),name="getProductBySubCategory"),
     path('products/search/<str:text>',views.GetProductByText.as_view(),name="GetProductByText")
--- shop/views.py
+++ shop/views.py
@@ -1,56 +1,93 @@
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
-from django.contrib.postgres.search import SearchQuery,SearchRank,SearchVector
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from .serialization import ProductSerializer
 from .models import Product
-from rest_framework.generics import ListAPIView,RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 # getting models
 from .models import Product
 # from rest_framework import BasicAuthentication
 from visualshop.utility.request import Success
 # Create your views here.
+
+
 class ProductPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
 
-
 class GetProductsAPI(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
+
+
 class GetProductDetail(RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
 class GetProductByCategory(ListAPIView):
     serializer_class = ProductSerializer
     model = Product
     pagination_class = ProductPagination
+
     def get_queryset(self):
         uid = self.kwargs.get('pk')
         queryset = self.model.objects.filter(subCategoryId__categoryId=uid)
         return queryset
+
+
 class GetProductBySubCategory(ListAPIView):
     serializer_class = ProductSerializer
     model = Product
     pagination_class = ProductPagination
+
     def get_queryset(self):
         category = self.kwargs.get('category')
-        subcategory=self.kwargs.get('subcategory')
-        queryset = self.model.objects.filter(subCategoryId=subcategory,subCategoryId__categoryId=category)
+        subcategory = self.kwargs.get('subcategory')
+        queryset = self.model.objects.filter(
+            subCategoryId=subcategory, subCategoryId__categoryId=category)
         return queryset
+
 
 class GetProductByText(ListAPIView):
     serializer_class = ProductSerializer
     model = Product
     pagination_class = ProductPagination
+
     def get_queryset(self):
         text = self.kwargs.get('text')
-        search_vector = SearchVector("tags__name",weight='A') + SearchVector('name', weight='B') + SearchVector('description', weight='C')
+        search_vector = SearchVector("tags__name", weight='A') + SearchVector(
+            'name', weight='B') + SearchVector('description', weight='C')
         search_query = SearchQuery(text)
         results = Product.objects.annotate(
             rank=SearchRank(search_vector, search_query)
         ).filter(rank__gte=0.3).order_by('-rank')
         return results
+class FilterProduct(APIView):
+    def get(self, request, format=None):
+        data = request.data
+        if data['searchText'] != None:
+            text = data['searchText']
+            search_vector = SearchVector("tags__name", weight='A') + SearchVector('name', weight='B') + SearchVector('description', weight='C')
+            search_query = SearchQuery(text)
+            products = Product.objects.annotate(
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
+        else:
+            products=Product.objects.all()
+        if data['price'] != None:
+            products = products.filter(
+                price__gte=data['price'][0], price__lte=data['price'][1])
+        if data['tags'] != None:
+            products = products.filter(tags__id__in=data['tags'])
+        if data['subcategoryId'] != None:
+            products = products.filter(subCategoryId=data['subcategoryId'])
+        elif data['categoryId'] != None:
+            products = products.filter(
+                subCategoryId__categoryId=data['categoryId'])
+        serializer=ProductSerializer(products,many=True)
+        return Success(serializer.data)
