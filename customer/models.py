0ab9c542c6317c701c0d0002ab2cbe81cf97646c
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-03-10 13:17:00 +0500

    password reset not working bug fixed

--- customer/models.py
+++ customer/models.py
@@ -1,23 +1,4 @@
 from customer.models.City import City
 from customer.models.Customer import Customer
 from customer.models.Province import Province
-# Reset password libraries
-from django.dispatch import receiver
-from django.urls import reverse
-from django_rest_passwordreset.signals import reset_password_token_created
-from django.core.mail import send_mail  
-
-# Signal to send email after password reset
-@receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
-    send_mail(
-        # title:
-        "Password Reset for {title}".format(title="Some website title"),
-        # message:
-        email_plaintext_message,
-        # from:
-        "noreply@visualshop.local",
-        # to:
-        [reset_password_token.user.email]
-    )
+ 
--- customer/models/Customer.py
+++ customer/models/Customer.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 from django.core.validators import  RegexValidator
 from customer.models.City import City
 from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
+from django.core.mail import send_mail  
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
@@ -21,3 +25,19 @@ def username_check(sender, instance, **kwargs):
     instance.username=instance.username.lower()
     instance.email=instance.email.lower()
 pre_save.connect(username_check, sender=User)
+
+
+# Signal to send email after password reset
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+    send_mail(
+        # title:
+        "Password Reset for {title}".format(title="Some website title"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "noreply@visualshop.local",
+        # to:
+        [reset_password_token.user.email]
+    )
