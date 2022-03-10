816ecfc3a6c23bb2b2793c81833c062c323e78de
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-03-10 13:25:04 +0500

    code refactoring

--- shop/models.py
+++ shop/models.py
@@ -1,28 +1,7 @@
 from django.db import models
-from django.dispatch import receiver
-import os
 from shop.models.Category import Category
 from shop.models.Images import Images
 from shop.models.Product import Product
 from shop.models.SubCategory import SubCategory
 from shop.models.Tags import Tags
 
-# ==================== Signals for Image deleted or updated =============
-@receiver(models.signals.post_delete, sender=Images)
-def auto_delete_file_on_delete(sender, instance, **kwargs):
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
-@receiver(models.signals.pre_save, sender=Images)
-def auto_delete_file_on_change(sender, instance, **kwargs):
-    if not instance.pk:
-        return False
-    try:
-        old_file = Images.objects.get(pk=instance.pk).image
-    except object.DoesNotExist:
-        return False
-    new_file = instance.image
-    if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
-            
--- shop/models/Images.py
+++ shop/models/Images.py
@@ -1,7 +1,30 @@
 from shop.models.Product import Product
 from django.db import models
+from django.dispatch import receiver
+import os
 
 class Images(models.Model):
     image=models.ImageField()
     productId=models.ForeignKey(Product,on_delete=models.CASCADE,null=False,blank=False,related_name='images',verbose_name='Product')
     imageColor=models.CharField(max_length=20,blank=True,null=True,verbose_name='Color')
+
+
+# ==================== Signals for Image deleted or updated =============
+@receiver(models.signals.post_delete, sender=Images)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
+@receiver(models.signals.pre_save, sender=Images)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+    try:
+        old_file = Images.objects.get(pk=instance.pk).image
+    except object.DoesNotExist:
+        return False
+    new_file = instance.image
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
+            
