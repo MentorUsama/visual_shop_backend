3bb7b8f6e10569f2157c30c6abb94f5b2d427587
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-03-17 23:46:10 +0500

    features model added

--- shop/models/Images.py
+++ shop/models/Images.py
@@ -12,19 +12,29 @@ class Images(models.Model):
 # ==================== Signals for Image deleted or updated =============
 @receiver(models.signals.post_delete, sender=Images)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
+    # File is deleted so also deleting the image
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
+    # Deleting the features related to that image
+
 @receiver(models.signals.pre_save, sender=Images)
 def auto_delete_file_on_change(sender, instance, **kwargs):
+    # Checking if file is going to be added first time
     if not instance.pk:
         return False
+    # Getting the old image
     try:
         old_file = Images.objects.get(pk=instance.pk).image
     except object.DoesNotExist:
         return False
+    # Deleting the old image
     new_file = instance.image
     if not old_file == new_file:
         if os.path.isfile(old_file.path):
             os.remove(old_file.path)
-            
+
+@receiver(models.signals.post_save, sender=Images)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    # File is saved
+    pass
--- shop/models/__init__.py
+++ shop/models/__init__.py
@@ -3,3 +3,4 @@ from shop.models.Images import Images
 from shop.models.Product import Product
 from shop.models.SubCategory import SubCategory
 from shop.models.Tags import Tags
+from shop.models.Features import Features
