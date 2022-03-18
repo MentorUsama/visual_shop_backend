734f247d5a46debdb88abd23b5da8601881912db
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-03-18 22:05:10 +0500

    pytorch removed

--- shop/core/utility/get_model_result.py
+++ shop/core/utility/get_model_result.py
@@ -1,19 +1,19 @@
-from torchvision import models
+# from torchvision import models
 from PIL import Image
-import torch
+# import torch
 from visualshop.settings import STATIC_ROOT
 from shop.core.utility.transform_image import transform
 def get_model_result(image):
     # Getting the pretrained model
-    alexnet = models.alexnet(pretrained=True)
+    # alexnet = models.alexnet(pretrained=True)
     # getting the uploaded image
     img=Image.open(image).convert('RGB')
     # transforming the image
     img_t = transform(img)
     # evaluation
-    batch_t = torch.unsqueeze(img_t, 0)
-    alexnet.eval()
-    out = alexnet(batch_t)
+    # batch_t = torch.unsqueeze(img_t, 0)
+    # alexnet.eval()
+    # out = alexnet(batch_t)
     # Getting the label of the output
     url = STATIC_ROOT + "/imagenet_classes.txt"
     classes=[]
@@ -23,8 +23,9 @@ def get_model_result(image):
             full_line=line.strip()
             label=full_line.split(',',1)[1]
             classes.append(label.strip())
-    _, index = torch.max(out, 1)
-    percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
-    _, indices = torch.sort(out, descending=True)
-    all_matches=[(classes[idx], percentage[idx].item()) for idx in indices[0][:5]] 
+    # _, index = torch.max(out, 1)
+    # percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    # _, indices = torch.sort(out, descending=True)
+    # all_matches=[(classes[idx], percentage[idx].item()) for idx in indices[0][:5]] 
+    all_matches = []
     return all_matches
