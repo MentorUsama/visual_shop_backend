4f72c1650511776c4e79a7474c063b37d4f4a29b
Muhammad Usama <61818692+MentorUsama@users.noreply.github.com>
2022-03-18 22:29:31 +0500

    signal removed

--- shop/views/signal_images.py
+++ shop/views/signal_images.py
@@ -9,41 +9,44 @@ from shop.core.features.write import bulk_create
 # ==================== Signals for Image deleted or updated =============
 @receiver(models.signals.post_delete, sender=Images)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
-    print("------------------------------ on delete ---------------------")
-    # File is deleted so also deleting the image
-    if instance.image:
-        if os.path.exists(instance.image.path):
-            os.remove(instance.image.path)
-    # Deleting the features related to that image
-    Features.objects.filter(imageId=instance.id).delete()
+    pass
+    # print("------------------------------ on delete ---------------------")
+    # # File is deleted so also deleting the image
+    # if instance.image:
+    #     if os.path.exists(instance.image.path):
+    #         os.remove(instance.image.path)
+    # # Deleting the features related to that image
+    # Features.objects.filter(imageId=instance.id).delete()
 
 @receiver(models.signals.pre_save, sender=Images)
 def auto_delete_file_on_change(sender, instance, **kwargs):
-    # Checking if file is going to be added first time
-    if not instance.pk:
-        return False
-    # Getting the old image
-    try:
-        old_file = Images.objects.get(pk=instance.pk).image
-    except object.DoesNotExist:
-        return False
-    # Deleting the old image
-    new_file = instance.image
-    if not old_file == new_file:
-        if os.path.exists(old_file.path):
-            os.remove(old_file.path)
+    pass
+    # # Checking if file is going to be added first time
+    # if not instance.pk:
+    #     return False
+    # # Getting the old image
+    # try:
+    #     old_file = Images.objects.get(pk=instance.pk).image
+    # except object.DoesNotExist:
+    #     return False
+    # # Deleting the old image
+    # new_file = instance.image
+    # if not old_file == new_file:
+    #     if os.path.exists(old_file.path):
+    #         os.remove(old_file.path)
 
 @receiver(models.signals.post_save, sender=Images)
 def image_on_save(sender, instance, **kwargs):
-    print("on_save")
-    # If features already exist for that image then deleting it
-    try:
-        feature_prev=Features.objects.filter(imageId=instance.id)
-        feature_prev.delete()
-    except Features.DoesNotExist:
-        feature_prev=None
-    # Adding the feature of image
-    all_features=get_model_result(instance.image)
+    pass
+    # print("on_save")
+    # # If features already exist for that image then deleting it
+    # try:
+    #     feature_prev=Features.objects.filter(imageId=instance.id)
+    #     feature_prev.delete()
+    # except Features.DoesNotExist:
+    #     feature_prev=None
+    # # Adding the feature of image
+    # all_features=get_model_result(instance.image)
     
-    if(len(all_features)!=0):
-        bulk_create(all_features,instance,instance.productId)
+    # if(len(all_features)!=0):
+    #     bulk_create(all_features,instance,instance.productId)
